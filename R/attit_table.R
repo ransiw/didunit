12aa183faf6816a451c4c43937394daca5bce90e
ransiw <t.ransi.weerasooriya@gmail.com>
2025-09-01 10:32:55 -0700

    update the attit_table to tabulate the results from a did MP object

--- R/attit_table.R
+++ R/attit_table.R
@@ -15,6 +15,10 @@ attit_table <- function(MP){
   #idname = MP$DIDparams$idname
   #gname = MP$DIDparams$gname
   #tname = MP$DIDPparams$tname
-  results = data.frame(id=MP$id,group=MP$group,t=MP$t,att=MP$att,lci=MP$lci,uci=MP$uci)
+  if (is.null(MP$c)){
+    results = data.frame(id=MP$id,group=MP$group,t=MP$t,att=MP$att,lci=MP$lci,uci=MP$uci)
+  } else{
+    results = data.frame(id=NA,group=MP$group,t=MP$t,att=MP$att,lci=MP$att-MP$c*MP$se,uci=MP$att+MP$c*MP$se)
+  }
   return(results)
 }
